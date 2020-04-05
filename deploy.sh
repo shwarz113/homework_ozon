c488f2de2af5dd39ece53f4e0dbe063be20c0b4f
Nikolai_Efimkin <nikolai_efimkin@epam.com>
2020-04-05 20:39:09 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,10 @@ npm run build
 
 cd dist
 
-# если вы публикуете по адресу https://<USERNAME>.github.io
-git push -f git@github.com:shwarz113/shwarz113.github.io.git master
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:shwarz113/homework_ozon.git master:pages
 
 cd -
